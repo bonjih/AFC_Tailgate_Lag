8ccf43e43628dfa2eb21c6a6b223baf3cea2a144
bonjih <bonjih@gmail.com>
2021-12-27 15:30:15 +1000

    Added VariableClass.py

---
+++ prod/VariableClass.py
@@ -0,0 +1,34 @@
+__author__ = "Ben Hamilton - Titan ICT Consultants"
+__email__ = "ben.hamilton@titanict.com.au"
+__phone__ = "+61 7 3360 4900"
+__license__ = "xxx"
+__version__ = "1.0.0"
+__maintainer__ = "Anglo American"
+__status__ = "Dev"
+
+
+class ImagesData:
+    """holds all database variables
+      values sent to the db_manager and then the db
+
+      date_time_db[0]  - date/time image added to db
+      cam_dist_to_gate[17] - distance in mm from bottom of image (camera) to tailgate (reference obj xB,yB)
+      chain_dist_to_gate[16] - chain_dist_to_gate in pixels
+      tailgate_coord_x[37] - tailgate_coord_x in image
+      tailgate_coord_y[38] - tailgate_coord_y in image
+      distance_lead[16] - tailgate distance leading the pan
+      distance_lag[15] - tailgate distance_lag
+      file_size[2]  - image file_size (on disk)
+      file_name[1]  - image name
+      num_pixels[3]  - image number of pixels
+      date_time_create[4]  - date/time image was created (take by camera)
+      image_id[5]  - id of image"""
+
+    def __init__(self, date_time_db, cam_dist_to_gate, chain_dist_to_gate, tailgate_coord_x, tailgate_coord_y,
+                 distance_lead,
+                 distance_lag, file_name, file_size, num_pixels, date_time_create, image_id):
+        self.date_time_db, self.cam_dist_to_gate, self.chain_dist_to_gate, self.tailgate_coord_x, self.tailgate_coord_y, \
+        self.distance_lead, self.distance_lag, self.file_name, self.file_size, self.num_pixels, self.date_time_create, \
+        self.image_id = date_time_db, cam_dist_to_gate, chain_dist_to_gate, tailgate_coord_x, tailgate_coord_y, \
+                        distance_lead, distance_lag, file_name, file_size, num_pixels, date_time_create, image_id
+        self.post_fix = None
--- prod/main.py
+++ prod/main.py
@@ -6,10 +6,14 @@ __version__ = "1.0.0"
 __maintainer__ = "Anglo American - email: "
 __status__ = "Dev"
 
+import shutil
 
 import image_detect
 import db_manager
 import cv_image_processing
+from prod.utils import image_compare
+import VariableClass
+
 import time
 import json
 import pymysql as pymysql  # to catch db errors
@@ -31,6 +35,7 @@ def config_json_parser():
         data = json.load(jsonFile)
         for key, value in data.items():
             config_values.append(value)
+
     return config_values
 
 
@@ -53,39 +58,23 @@ def image_detect_controller():
 # formats all variable to for processing the db
 def format_image_data(img_data):
     cv_data_tup = cv_image_processing.cv_processing(img_data)
-
     cv_data = cv_data_tup[0] + cv_data_tup[1]  # concatenate cv_data list, pass to db_manager.image_data()
     img_meta_data = image_detect_controller()
     image_data = list(img_meta_data) + cv_data
 
-    # values sent to the db_manager and then the db
-
-    # image_data[0]  - date/time image added to dir
-    # image_data[15] - distance in mm from bottom of image (camera) to tailgate (reference obj xA,xB)
-    # image_data[20] - cam_dist_to_gate in pixels
-    # image_data[35] - tailgate_coord_x in image
-    # image_data[36] - tailgate_coord_y in image
-    # image_data[16]  - tailgate distance leading the pan
-    # image_data[9]  - tailgate distance_lag
-    # image_data[2]  - image file_size (on disk)
-    # image_data[1]  - image name
-    # image_data[3]  - image number of pixels
-    # image_data[4]  - date/time image was created (take by camera)
-    # image_data[5]  - id of image
-
-    image_data2 = (image_data[0], image_data[15], image_data[20], image_data[35], image_data[36], image_data[16],
-                   image_data[9], image_data[2], image_data[1], image_data[3], image_data[4], image_data[5])
+    image_data2 = VariableClass.ImagesData(image_data[0], image_data[17], image_data[16], image_data[37],
+                                           image_data[38], image_data[16], image_data[15], image_data[2],
+                                           image_data[1], image_data[3], image_data[4], image_data[5])
 
     return image_data2, img_meta_data
 
 
 def db_manager_controller(dbfields):
-    dir_img_name = image_detect.img_meta_data(jconfigs)
+    image_date = image_detect.img_meta_data(jconfigs)
     db_manager.db_creds_json(jconfigs)
     db_manager.db_connect()
 
-    exists = db_manager.check_entry_exist(dir_img_name)
-
+    exists = db_manager.check_entry_exist(image_date)
     image_data2, img_meta_data = format_image_data(jconfigs)
 
     if not exists:
@@ -101,6 +90,7 @@ if __name__ == "__main__":
         print('Checking for latest image.....')
         try:
             jconfigs = config_json_parser()
+            image_compare.main(jconfigs)
             image_detect_controller()
             cv_image_processing.load_image(jconfigs)
             db_fields = db_json_parser()
@@ -111,8 +101,8 @@ if __name__ == "__main__":
                 e))
             time.sleep(alarm_delay)
         except ValueError as e:
-            print("Error in 'jconfig.json' format or cannot find an image or extension is incorrect. Please check "
-                  "config.json: {}".format(e))
+            print("Error in 'jconfig.json' format or cannot find an image or extension is incorrect or no file in dir "
+                  "'filtered'. Please check config.json and dir 'filtered: {}".format(e))
             time.sleep(alarm_delay)
         except AttributeError as e:
             print('Error: {}'.format(e))
@@ -129,6 +119,7 @@ if __name__ == "__main__":
             print("Database connection restarted. Error: {}".format(e))
         except sqlalchemy.exc.OperationalError as e:
             print("There is a mismatch between table names in db_fields.json and the database.")
-
-
-
+        except PermissionError as e:
+            print("Read/Write: {}".format(e))
+        except shutil.SameFileError as e:
+            print("Some Err: {}".format(e))
