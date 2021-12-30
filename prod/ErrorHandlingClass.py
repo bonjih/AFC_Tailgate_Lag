95424c5fc03deda29ff04decb692452715f5423d
bonjih <bonjih@gmail.com>
2021-12-30 18:23:32 +1000

    Updated error messages

--- prod/ErrorHandlingClass.py
+++ prod/ErrorHandlingClass.py
@@ -7,6 +7,10 @@ __maintainer__ = "Anglo American"
 __status__ = "Dev"
 
 import time
+import pymysql as pymysql  # to catch db errors
+from sqlalchemy import exc  # to catch db insert errors
+import sqlalchemy  # to catch db insert errors
+import shutil
 
 # controls the error message sent interval in seconds
 alarm_delay = 5
@@ -16,51 +20,32 @@ class ErrorMessageHandler:
     """ Error messages"""
 
     def __init__(self, error):
-        err_type = str(type(error))
-        err_type = (err_type[8:-2])
-
-        if err_type == 'FileNotFoundError':
-            print("Can not reach 'jconfig.json' or path to image, please check path to image or config.json: {}".format(
-                    error))
-            time.sleep(alarm_delay)
-
-        elif err_type == 'ValueError':
+        try:
+            raise error
+        except FileNotFoundError as error:
             print("Can not reach 'jconfig.json' or path to image, please check path to image or config.json: {}".format(
                 error))
             time.sleep(alarm_delay)
-
-        elif err_type == 'AttributeError':
-            print('Error: {}'.format(error))
+        except ValueError as error:
+            print("Error in 'jconfig.json' format or cannot find an image or extension is incorrect or no file in dir "
+                  "'filtered'. Please check config.json and dir 'filtered: {}".format(error))
             time.sleep(alarm_delay)
-
-        elif err_type == 'TypeError':
-            print('Error: {}'.format(error))
+        except AttributeError as e:
+            print('Error: {}'.format(e))
             time.sleep(alarm_delay)
-
-        elif err_type == 'pymysql.OperationalError':
-            print('No connection to database. Please check connection details in config.json: {}'.format(error))
+        except TypeError as e:
+            print("Error: {}".format(e))
             time.sleep(alarm_delay)
-
-        elif err_type == 'KeyboardInterrupt':
+        except pymysql.OperationalError as e:
+            print('No connection to database. Please check connection details in config.json: {}'.format(e))
+            time.sleep(alarm_delay)
+        except KeyboardInterrupt:
             print('\n! Received interrupt, quitting threads, restart main.py, check path to image in config.json.\n')
-            time.sleep(alarm_delay)
-
-        elif err_type == 'ConnectionResetError':
-            print("Database connection restarted. Error: {}".format(error))
-            time.sleep(alarm_delay)
-
-        elif err_type == 'sqlalchemy.exc.OperationalError':
+        except ConnectionResetError as e:
+            print("Database connection restarted. Error: {}".format(e))
+        except sqlalchemy.exc.OperationalError as e:
             print("There is a mismatch between table names in db_fields.json and the database.")
-            time.sleep(alarm_delay)
-
-        elif err_type == 'PermissionError':
-            print("Read/Write: {}".format(error))
-            time.sleep(alarm_delay)
-
-        elif err_type == 'shutil.SameFileError':
-            print("Some Error with 'image_compare.py': {}".format(error))
-            time.sleep(alarm_delay)
-
-        elif err_type == 'shutil.Error':
-            print("Some Error with 'image_compare.py': {}".format(error))
-            time.sleep(alarm_delay)
+        except PermissionError as e:
+            print("Read/Write: {}".format(e))
+        except shutil.SameFileError as e:
+            print("Some Err: {}".format(e))
