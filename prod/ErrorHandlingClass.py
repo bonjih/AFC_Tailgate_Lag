d2a73e728d20dd5c52553445488715894c37f938
bonjih <bonjih@gmail.com>
2022-01-05 16:48:48 +1000

    Updated error messages, added image variable

--- prod/ErrorHandlingClass.py
+++ prod/ErrorHandlingClass.py
@@ -11,9 +11,15 @@ import pymysql as pymysql  # to catch db errors
 from sqlalchemy import exc  # to catch db insert errors
 import sqlalchemy  # to catch db insert errors
 import shutil
-from prod.utils import Logger
 
-# controls the error message sent interval in seconds
+from prod import VariableClass
+from prod.utils import Logger
+from main import config_json_parser
+
+config = config_json_parser()
+image_data = VariableClass.img_meta_data(config)
+
+
 alarm_delay = 5
 
 
@@ -21,24 +27,28 @@ class ErrorMessageHandler:
     """ Error messages"""
 
     def __init__(self, error):
+
         try:
             raise error
         except FileNotFoundError as error:
-            print("Can not reach 'jconfig.json' or path to image, please check path to image or config.json: {}".format(
+            s = Logger.ErrorLog("Can not reach 'jconfig.json' or path to image, please check path to image or "
+                                "config.json: {}".format(
                 error))
+            s.show()
             time.sleep(alarm_delay)
         except ValueError as error:
             s = Logger.ErrorLog("Error in 'jconfig.json' format or cannot find an image or extension is incorrect "
                                 "or no file in dir 'filtered'. Please check config.json and dir 'filtered: {}".format(
                 error))
+            s.show()
+            time.sleep(alarm_delay)
+        except AttributeError as error:
+            s = Logger.ErrorLog('Error: {}'.format(error))
             time.sleep(alarm_delay)
             s.show()
-
-        except AttributeError as error:
-            print('Error: {}'.format(error))
-            time.sleep(alarm_delay)
         except TypeError as error:
-            print("Error: {}".format(error))
+            s = Logger.ErrorLog("Usually means image {} lacks detail for processing. Error: {}".format(image_data[1], error))
+            s.show()
             time.sleep(alarm_delay)
         except pymysql.OperationalError as error:
             print('No connection to database. Please check connection details in config.json: {}'.format(error))
@@ -53,3 +63,6 @@ class ErrorMessageHandler:
             print("Read/Write: {}".format(error))
         except shutil.SameFileError as error:
             print("Some Err: {}".format(error))
+        except OSError as error:
+            print("Some Err: {}".format(error))
+
