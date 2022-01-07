07ad9eaa57f80ee1b1f0c1af949becbde84c042f
bonjih <bonjih@gmail.com>
2022-01-08 09:29:25 +1000

    Add log processing function

--- prod/ErrorHandlingClass.py
+++ prod/ErrorHandlingClass.py
@@ -30,50 +30,38 @@ class ErrorMessageHandler:
         try:
             raise error
         except FileNotFoundError as error:
-            s = LoggerClass.ErrorLog("Can not reach 'jconfig.json' or path to image: {}".format(
-                error))
-            s.show()
+            log_processing("Can not reach 'jconfig.json' or path to image: {}".format(error))
             time.sleep(alarm_delay)
         except ValueError as error:
-            s = LoggerClass.ErrorLog(
-                "Error in 'jconfig.json' format or cannot find an image or extension is incorrect, "
-                "or no file in dir 'filtered'. Please check config.json and dir 'filtered: {}".format(
-                    error))
-            s.show()
+            log_processing("Error in 'jconfig.json' format or cannot find an image or extension is incorrect, or no "
+                           "file in dir 'filtered'. Check config.json and dir 'filtered: {}".format(error))
             time.sleep(alarm_delay)
         except AttributeError as error:
-            s = LoggerClass.ErrorLog('Error: {}'.format(error))
+            log_processing('Error: {}'.format(error))
             time.sleep(alarm_delay)
-            s.show()
         except TypeError as error:
-            s = LoggerClass.ErrorLog(
-                "Usually means image {} lacks detail for processing. Error: {}".format(image_data[1], error))
-            s.show()
+            log_processing("Usually means image {} lacks detail for processing. Error: {}".format(image_data[1], error))
             time.sleep(alarm_delay)
         except pymysql.OperationalError as error:
-            s = LoggerClass.ErrorLog(
-                'No connection to database. Please check connection details in config.json: {}'.format(error))
-            s.show()
+            log_processing('No connection to database. Check connection details in config.json: {}'.format(error))
             time.sleep(alarm_delay)
         except KeyboardInterrupt:
-            s = LoggerClass.ErrorLog(
-                '\n! Received interrupt, quitting threads, restart main.py, check path to image in config.json.\n')
-            s.show()
+            log_processing('\n! Received interrupt, quitting threads, restart main.py, check path to image in '
+                           'jconfig.json.\n')
         except ConnectionResetError as error:
-            s = LoggerClass.ErrorLog("Database connection restarted. Error: {}".format(error))
-            s.show()
+            log_processing("Database connection restarted. Error: {}".format(error))
         except sqlalchemy.exc.OperationalError:
-            s = LoggerClass.ErrorLog("There is a mismatch between table names in db_fields.json and the database.")
-            s.show()
+            log_processing("There is a mismatch between table names in db_fields.json and the database.")
         except PermissionError as error:
-            s = LoggerClass.ErrorLog("Read/Write: {}".format(error))
-            s.show()
+            log_processing("Read/Write: {}".format(error))
         except shutil.SameFileError as error:
-            s = LoggerClass.ErrorLog("Some Err: {}".format(error))
-            s.show()
+            log_processing("Some Err: {}".format(error))
         except OSError as error:
-            s = LoggerClass.ErrorLog("Can't access the Gelphotos directory: {}".format(error))
-            s.show()
+            log_processing("Can't access the Gelphotos directory: {}".format(error))
         except Exception as error:
-            s = LoggerClass.ErrorLog("Catch all error and log in [./log]: {}".format(error))
-            s.show()
+            log_processing("Catch all error and log in [./log]: {}".format(error))
+
+
+def log_processing(message):
+    s = LoggerClass.ErrorLog(message)
+    s.show()
